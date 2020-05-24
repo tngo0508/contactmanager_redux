1c53dae8b131a75b7789b313a1b3d39f42a601f0
Thomas Ngo <tngo0508@gmai.com>
2020-05-23 20:03:09 -0700

    POST & DELETE request dispatch

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,21 +1,35 @@
 import { GET_CONTACTS, DELETE_CONTACTS, ADD_CONTACTS } from './types';
+import axios from 'axios';
 
-export const getContacts = () => {
-  return {
-    type: GET_CONTACTS
-  };
+export const getContacts = () => async dispatch => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
 };
 
-export const deleteContacts = (id) => {
-  return {
-    type: DELETE_CONTACTS,
-    payload: id
-  };
+export const deleteContacts = (id) => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    dispatch({
+      type: DELETE_CONTACTS,
+      payload: id
+    });
+  } catch (e) {
+    dispatch({
+      type: DELETE_CONTACTS,
+      payload: id
+    });
+  }
+
 };
 
-export const addContacts = (contact) => {
-  return {
+export const addContacts = (contact) => async dispatch => {
+  const res = await axios.post('https://jsonplaceholder.typicode.com/users', contact);
+  dispatch({
     type: ADD_CONTACTS,
-    payload: contact
-  };
+    payload: res.data
+  });
 };
--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,7 +3,7 @@ import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addContacts } from '../../actions/contactActions';
-import { v1 as uuid } from 'uuid';
+// import { v1 as uuid } from 'uuid';
 
 class AddContact extends Component {
   state = {
@@ -35,7 +35,7 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
+      // id: uuid(),
       name,
       email,
       phone,
