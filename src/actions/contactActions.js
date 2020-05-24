e6608e4eaedc435a65c63dcacad8c76fc08e5867
Thomas Ngo <tngo0508@gmai.com>
2020-05-23 20:32:50 -0700

    Get Single Contact for edit

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,35 +1,53 @@
-import { GET_CONTACTS, DELETE_CONTACTS, ADD_CONTACTS } from './types';
-import axios from 'axios';
+import {
+  GET_CONTACTS,
+  DELETE_CONTACTS,
+  ADD_CONTACTS,
+  GET_CONTACT,
+  UPDATE_CONTACTS,
+} from "./types";
+import axios from "axios";
 
-export const getContacts = () => async dispatch => {
-  const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+export const getContacts = () => async (dispatch) => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
   dispatch({
     type: GET_CONTACTS,
-    payload: res.data
+    payload: res.data,
   });
 };
 
-export const deleteContacts = (id) => async dispatch => {
+export const getContact = (id) => async (dispatch) => {
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  dispatch({
+    type: GET_CONTACT,
+    payload: res.data,
+  });
+};
+
+export const deleteContacts = (id) => async (dispatch) => {
   try {
     await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
 
     dispatch({
       type: DELETE_CONTACTS,
-      payload: id
+      payload: id,
     });
   } catch (e) {
     dispatch({
       type: DELETE_CONTACTS,
-      payload: id
+      payload: id,
     });
   }
-
 };
 
-export const addContacts = (contact) => async dispatch => {
-  const res = await axios.post('https://jsonplaceholder.typicode.com/users', contact);
+export const addContacts = (contact) => async (dispatch) => {
+  const res = await axios.post(
+    "https://jsonplaceholder.typicode.com/users",
+    contact
+  );
   dispatch({
     type: ADD_CONTACTS,
-    payload: res.data
+    payload: res.data,
   });
 };
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,26 +1,15 @@
-import { GET_CONTACTS, DELETE_CONTACTS, ADD_CONTACTS } from '../actions/types';
+import {
+  GET_CONTACTS,
+  DELETE_CONTACTS,
+  ADD_CONTACTS,
+  GET_CONTACT,
+  UPDATE_CONTACTS,
+} from "../actions/types";
+import { bindActionCreators } from "redux";
 
 const initialState = {
-  contacts: [
-    {
-      id: 1,
-      name: 'John Doe',
-      email: 'john@gmail.com',
-      phone: '555-555-5555'
-    },
-    {
-      id: 2,
-      name: 'Karen Williams',
-      email: 'karen@gmail.com',
-      phone: '444-444-4444'
-    },
-    {
-      id: 3,
-      name: 'Henry Johnson',
-      email: 'henry@gmail.com',
-      phone: '333-333-333'
-    }
-  ]
+  contacts: [],
+  contact: {},
 };
 
 export default function (state = initialState, action) {
@@ -28,17 +17,24 @@ export default function (state = initialState, action) {
     case GET_CONTACTS:
       return {
         ...state,
-        contacts: action.payload
+        contacts: action.payload,
+      };
+    case GET_CONTACT:
+      return {
+        ...state,
+        contact: action.payload,
       };
     case DELETE_CONTACTS:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
       };
     case ADD_CONTACTS:
       return {
         ...state,
-        contacts: [action.payload, ...state.contacts]
+        contacts: [action.payload, ...state.contacts],
       };
     default:
       return state;
